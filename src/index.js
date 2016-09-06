81f46b52ef2ce98eacd181e67bd3d5551f02fd4a
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2016-09-06 17:41:25 +0530

    test Hot Reload Working - React state preseve - Dan

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("Hi");
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+	constructor(props, context) {
+		super(props, context);
+
+		this.state = {
+			counter: 0
+		};
+
+	  	var _this = this;
+	  	var counter = 0;
+
+	  	setInterval(function() {
+	  		_this.setState({
+	  			"counter": counter++
+	  		})
+	  	}, 1000)
+	}
+
+  render() {
+    return (
+    <div>
+      <h1>Hello, World.{this.state.counter}</h1>
+      <h2>Preserving State, Everytime I update</h2>
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
