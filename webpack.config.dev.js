84662b678f05ccdd0690c439dc4c76890dcbacad
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2016-09-12 15:14:27 +0530

    Bootstrap integration

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,14 @@
-var path = require("path");
-var webpack = require("webpack");
+import webpack from 'webpack';
+import path from 'path';
 
-module.exports = {
+export default {
 	debug: true,
-	devtool: "eval",
+	devtool: 'cheap-module-eval-source-map',
+	noInfo: false,
 	entry: [
 		// 'eventsource-polyfill', // necessary for hot reloading with IE
 		'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-		'./src/index'
+		path.resolve(__dirname, 'src/index')
 	],
 	target: 'web',
 	output: {
@@ -16,20 +17,32 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	devServer: {
-		contentBase: './src'
+		contentBase: path.resolve(__dirname, 'src')
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin()
 	],
 	module: {
-		loaders: [
-			{test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-			{test: /(\.css)$/, loaders: ['style', 'css']},
-			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-			{test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-			{test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-			{test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-		]
+		loaders: [{
+			test: /\.js$/,
+			include: path.join(__dirname, 'src'),
+			loaders: ['babel']
+		}, {
+			test: /(\.css)$/,
+			loaders: ['style', 'css']
+		}, {
+			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'file'
+		}, {
+			test: /\.(woff|woff2)$/,
+			loader: 'url?prefix=font/&limit=5000'
+		}, {
+			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'url?limit=10000&mimetype=application/octet-stream'
+		}, {
+			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+			loader: 'url?limit=10000&mimetype=image/svg+xml'
+		}]
 	}
 };
